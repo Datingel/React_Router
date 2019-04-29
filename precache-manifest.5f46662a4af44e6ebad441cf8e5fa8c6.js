6bc55c00a2ea202a4b879b0da5d2eb7ab598690b
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-04-29 19:34:34 +0200

    Updates

---
+++ precache-manifest.5f46662a4af44e6ebad441cf8e5fa8c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea6ffaf762334391f41d919d5b85dc6",
+    "url": "/React_Router/index.html"
+  },
+  {
+    "revision": "f46ad620ed20437dfb26",
+    "url": "/React_Router/static/css/main.547b6277.chunk.css"
+  },
+  {
+    "revision": "d03e09334e7ec478f63b",
+    "url": "/React_Router/static/js/2.5ebb96d8.chunk.js"
+  },
+  {
+    "revision": "f46ad620ed20437dfb26",
+    "url": "/React_Router/static/js/main.d3736657.chunk.js"
+  },
+  {
+    "revision": "e7ada3219007dae20f21",
+    "url": "/React_Router/static/js/runtime~main.c1acb98d.js"
+  }
+]);
